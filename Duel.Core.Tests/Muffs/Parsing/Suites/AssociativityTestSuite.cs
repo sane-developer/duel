438546c1cc5bddab25a9ab7a9e400d28ecebd4c1
Makyo <121882883+sane-developer@users.noreply.github.com>
2025-10-03 00:35:42 +0200

    Reworked AssociativityTest suite.

--- Duel.Core.Tests/Muffs/Parsing/Suites/AssociativityTestSuite.cs
+++ Duel.Core.Tests/Muffs/Parsing/Suites/AssociativityTestSuite.cs
@@ -6,56 +6,192 @@ namespace Duel.Core.Tests.Muffs.Parsing.Suites;
 public sealed class AssociativityTestSuite : ExpressionParserTestSuite
 {
     [Test]
-    public void Associativity_Left_SubtractionChainsLeft()
+    public void Associativity_Addition_LeftAssociative()
     {
-        const string expression = "10 - 3 - 2";
+        const string expression = "1 + 2 + 3 + 4";
         
         var result = GetExpression(expression);
 
-        var outer = IsValidBinary<Subtraction>(result);
-
-        var inner = IsValidBinary<Subtraction>(outer.Left);
+        var add4 = IsValidBinary<Addition>(result);
         
-        IsValidConstant(inner.Left, value: 10);
-        
-        IsValidConstant(inner.Right, value: 3);
+        IsValidConstant(add4.Right, value: 4);
 
-        IsValidConstant(outer.Right, value: 2);
+        var add3 = IsValidBinary<Addition>(add4.Left);
+        
+        IsValidConstant(add3.Right, value: 3);
+
+        var add2 = IsValidBinary<Addition>(add3.Left);
+        
+        IsValidConstant(add2.Left, value: 1);
+        
+        IsValidConstant(add2.Right, value: 2);
     }
 
     [Test]
-    public void Associativity_Left_DivisionChainsLeft()
+    public void Associativity_Subtraction_LeftAssociative()
     {
-        const string expression = "20 / 5 / 2";
+        const string expression = "10 - 3 - 2 - 1";
         
         var result = GetExpression(expression);
 
-        var outer = IsValidBinary<Division>(result);
-
-        var inner = IsValidBinary<Division>(outer.Left);
+        var sub1 = IsValidBinary<Subtraction>(result);
         
-        IsValidConstant(inner.Left, value: 20);
-        
-        IsValidConstant(inner.Right, value: 5);
+        IsValidConstant(sub1.Right, value: 1);
 
-        IsValidConstant(outer.Right, value: 2);
+        var sub2 = IsValidBinary<Subtraction>(sub1.Left);
+        
+        IsValidConstant(sub2.Right, value: 2);
+
+        var sub3 = IsValidBinary<Subtraction>(sub2.Left);
+        
+        IsValidConstant(sub3.Left, value: 10);
+        
+        IsValidConstant(sub3.Right, value: 3);
     }
 
     [Test]
-    public void Associativity_Right_PowerChainsRight()
+    public void Associativity_Multiplication_LeftAssociative()
     {
-        const string expression = "2 ^ 3 ^ 4";
+        const string expression = "2 * 3 * 4 * 5";
         
         var result = GetExpression(expression);
 
-        var outer = IsValidBinary<Power>(result);
+        var mul5 = IsValidBinary<Multiplication>(result);
         
-        IsValidConstant(outer.Left, value: 2);
+        IsValidConstant(mul5.Right, value: 5);
 
-        var inner = IsValidBinary<Power>(outer.Right);
+        var mul4 = IsValidBinary<Multiplication>(mul5.Left);
+        
+        IsValidConstant(mul4.Right, value: 4);
+
+        var mul3 = IsValidBinary<Multiplication>(mul4.Left);
+        
+        IsValidConstant(mul3.Left, value: 2);
+        
+        IsValidConstant(mul3.Right, value: 3);
+    }
+
+    [Test]
+    public void Associativity_Division_LeftAssociative()
+    {
+        const string expression = "100 / 5 / 2 / 5";
+        
+        var result = GetExpression(expression);
+
+        var div5 = IsValidBinary<Division>(result);
+        
+        IsValidConstant(div5.Right, value: 5);
+
+        var div2 = IsValidBinary<Division>(div5.Left);
+        
+        IsValidConstant(div2.Right, value: 2);
+
+        var div5A = IsValidBinary<Division>(div2.Left);
+        
+        IsValidConstant(div5A.Left, value: 100);
+        
+        IsValidConstant(div5A.Right, value: 5);
+    }
+
+    [Test]
+    public void Associativity_AddSub_MixedSamePrecedence_LeftAssociative()
+    {
+        const string expression = "10 - 3 + 2 - 1";
+        
+        var result = GetExpression(expression);
+
+        var sub1 = IsValidBinary<Subtraction>(result);
+        
+        IsValidConstant(sub1.Right, value: 1);
+
+        var add2 = IsValidBinary<Addition>(sub1.Left);
+        
+        IsValidConstant(add2.Right, value: 2);
+
+        var sub3 = IsValidBinary<Subtraction>(add2.Left);
        
-        IsValidConstant(inner.Left, value: 3);
+        IsValidConstant(sub3.Left, value: 10);
         
-        IsValidConstant(inner.Right, value: 4);
+        IsValidConstant(sub3.Right, value: 3);
+    }
+
+    [Test]
+    public void Associativity_MulDiv_MixedSamePrecedence_LeftAssociative()
+    {
+        const string expression = "40 / 5 * 2 / 4";
+        
+        var result = GetExpression(expression);
+
+        var div4 = IsValidBinary<Division>(result);
+        
+        IsValidConstant(div4.Right, value: 4);
+
+        var mul2 = IsValidBinary<Multiplication>(div4.Left);
+        
+        IsValidConstant(mul2.Right, value: 2);
+
+        var div5 = IsValidBinary<Division>(mul2.Left);
+        
+        IsValidConstant(div5.Left, value: 40);
+        
+        IsValidConstant(div5.Right, value: 5);
+    }
+
+    [Test]
+    public void Associativity_Power_RightAssociative_ThreeChain()
+    {
+        const string expression = "2 ^ 3 ^ 2";
+        
+        var result = GetExpression(expression);
+
+        var powOuter = IsValidBinary<Power>(result);
+        
+        IsValidConstant(powOuter.Left, value: 2);
+
+        var powInner = IsValidBinary<Power>(powOuter.Right);
+        
+        IsValidConstant(powInner.Left, value: 3);
+        
+        IsValidConstant(powInner.Right, value: 2);
+    }
+
+    [Test]
+    public void Associativity_Power_RightAssociative_FourChain()
+    {
+        const string expression = "2 ^ 3 ^ 2 ^ 1";
+        
+        var result = GetExpression(expression);
+
+        var powA = IsValidBinary<Power>(result);
+        
+        IsValidConstant(powA.Left, value: 2);
+
+        var powB = IsValidBinary<Power>(powA.Right);
+        
+        IsValidConstant(powB.Left, value: 3);
+
+        var powC = IsValidBinary<Power>(powB.Right);
+        
+        IsValidConstant(powC.Left, value: 2);
+        
+        IsValidConstant(powC.Right, value: 1);
+    }
+
+    [Test]
+    public void Associativity_Power_WithParentheses_OverridesRightAssociativity()
+    {
+        const string expression = "(2 ^ 3) ^ 2";
+        
+        var result = GetExpression(expression);
+
+        var powOuter = IsValidBinary<Power>(result);
+        
+        IsValidConstant(powOuter.Right, value: 2);
+
+        var powInner = IsValidBinary<Power>(powOuter.Left);
+        
+        IsValidConstant(powInner.Left, value: 2);
+        
+        IsValidConstant(powInner.Right, value: 3);
     }
 }
