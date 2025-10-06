38838052648fcd90310897832f25a6a1745107e9
Makyo <121882883+sane-developer@users.noreply.github.com>
2025-10-06 17:24:26 +0200

    Restructured the project.

--- src/Duel.API/Program.cs
+++ src/Duel.API/Program.cs
@@ -1,11 +1,12 @@
-using Duel.Web;
-
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
@@ -34,10 +35,7 @@ app.MapGet("/weatherforecast", () =>
 
 app.Run();
 
-namespace Duel.Web
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
-    record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-    {
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-    }
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
